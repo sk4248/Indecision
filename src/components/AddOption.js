e2e027afab39595df866f02ae677dc0d5c97755f
sreekanth <sreekanth@call-em-all.com>
2018-09-11 15:27:18 -0500

    Indecision app is being built succesfully

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,17 +1,10 @@
 
 import React from 'react';
 
-
-
 export default class AddOption extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleAddOption = this.handleAddOption.bind(this);
-      this.state = {
-        error: undefined
-      };
-    }
-    handleAddOption(e) {
+  state ={ error: undefined}
+  
+    handleAddOption = (e) =>{
       e.preventDefault();
   
       const option = e.target.elements.option.value.trim();
@@ -26,10 +19,10 @@ export default class AddOption extends React.Component {
     render() {
       return (
         <div>
-          {this.state.error && <p>{this.state.error}</p>}
-          <form onSubmit={this.handleAddOption}>
-            <input type="text" name="option" />
-            <button>Add Option</button>
+          {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+          <form className='add-option' onSubmit={this.handleAddOption}>
+            <input className='add-option__input' type="text" name="option" />
+            <button className='button'>Add Option</button>
           </form>
         </div>
       );
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-
- const Option = (props) => {
-    return (
-      <div>
-        {props.optionText}
+const Option = (props) =>(
+      <div className="option">
+      <p className="option__text">{props.count}.  {props.optionText}</p>
         <button
+          className= 'button button--link'
           onClick={(e) => {
             props.handleDeleteOption(props.optionText);
           }}
@@ -14,6 +13,5 @@ import React from 'react';
         </button>
       </div>
     );
-  };
 
   export default Option;
